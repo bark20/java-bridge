d62827448c157ec79f33c7f940703f19e00a937e
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-26 17:12:01 +0900

    refactor(Bridge) : 다리 길이 검증 로직 수정

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -19,7 +19,7 @@ public class Bridge {
     }
 
     private static void validateBridgeSize(int bridgeSize) {
-        if (!(bridgeSize >= MIN_BRIDGE_SIZE && bridgeSize <= MAX_BRIDGE_SIZE)) {
+        if (bridgeSize < MIN_BRIDGE_SIZE || bridgeSize > MAX_BRIDGE_SIZE) {
             throw new IllegalArgumentException(ERROR_PREFIX + "다리의 길이는 3이상 20이하의 숫자만 입력할 수 있습니다.");
         }
     }
