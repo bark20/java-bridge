411f2fe591d379f9743d26aa361f763321e3a0f6
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-26 16:56:43 +0900

    feat(GameValue) : 다리건너기 게임 공통값 상수화

---
+++ src/main/java/bridge/constants/GameValue.java
@@ -0,0 +1,14 @@
+package bridge.constants;
+
+public class GameValue {
+
+    private GameValue() {
+    }
+
+    public static final String MOVE_UP_COMMAND = "U";
+    public static final String MOVE_DOWN_COMMAND = "D";
+    public static final String RETRY_COMMAND = "R";
+    public static final String QUIT_COMMAND = "Q";
+    public static final String MOVE = "O";
+    public static final String STOP = "X";
+}
