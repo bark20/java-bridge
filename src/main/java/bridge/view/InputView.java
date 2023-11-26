0d4690bb882764ab6a1a85da5d528404d2ae44bc
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-26 14:09:03 +0900

    feat(InputView, InputValidator) : 재시작/ 종료 여부 입력 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -32,6 +32,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String input = Console.readLine();
+        InputValidator.validateRetryCommand(input);
+        return input;
     }
 }
--- src/main/java/bridge/view/validator/InputValidator.java
+++ src/main/java/bridge/view/validator/InputValidator.java
@@ -4,6 +4,8 @@ public class InputValidator {
     private static final String ERROR_PREFIX = "[ERROR] ";
     private static final String MOVE_UP_COMMAND = "U";
     private static final String MOVE_DOWN_COMMAND = "D";
+    private static final String RETRY_COMMAND = "R";
+    private static final String QUIT_COMMAND = "Q";
     private static final int MIN_BRIDGE_SIZE = 3;
     private static final int MAX_BRIDGE_SIZE = 20;
 
@@ -19,6 +21,12 @@ public class InputValidator {
     public static void validateMoveCommand(String command) {
         if (!command.equals(MOVE_UP_COMMAND) && !command.equals(MOVE_DOWN_COMMAND)) {
             throw new IllegalArgumentException(ERROR_PREFIX + "이동할 칸은 U 또는 D 문자만 입력할 수 있습니다.");
+        }
+    }
+
+    public static void validateRetryCommand(String command) {
+        if (!command.equals(RETRY_COMMAND) && !command.equals(QUIT_COMMAND)) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "재시작/종료는 R 또는 Q 문자만 입력할 수 있습니다.");
         }
     }
 }
