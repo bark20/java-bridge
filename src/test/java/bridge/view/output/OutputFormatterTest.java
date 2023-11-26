d2ad4cdc0509e5bade0874d3b87666daa4da7c6d
haero77 <shineecard5@gmail.com>
2023-11-26 17:39:45 +0900

    test: 미사용 import 제거

--- src/test/java/bridge/view/output/OutputFormatterTest.java
+++ src/test/java/bridge/view/output/OutputFormatterTest.java
@@ -1,12 +1,10 @@
 package bridge.view.output;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import bridge.domain.MovePosition;
 import bridge.domain.MoveRecord;
 import bridge.domain.MovingDirection;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
