94fb58efc2ea8b56ec4cd6987b862c2642e53277
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-26 17:10:33 +0900

    refactor(BridgeMaker) : 다리 생성 로직 기능 분리

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -7,7 +7,6 @@ import java.util.List;
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -19,16 +18,34 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        List<String> bridge = new ArrayList<>(size);
+        List<String> bridge = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            int bridgeNumber = bridgeNumberGenerator.generate();
-            if (bridgeNumber == 0) {
-                bridge.add("D");
-            }
-            if (bridgeNumber == 1) {
-                bridge.add("U");
-            }
+            addBridge(bridge);
         }
         return bridge;
+    }
+
+    private void addBridge(List<String> bridge) {
+        int bridgeNumber = bridgeNumberGenerator.generate();
+        if (isUpper(bridgeNumber)) {
+            bridge.add("U");
+        }
+        if (isLower(bridgeNumber)) {
+            bridge.add("D");
+        }
+    }
+
+    private boolean isUpper(int bridgeNumber) {
+        if (bridgeNumber == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isLower(int bridgeNumber) {
+        if (bridgeNumber == 0) {
+            return true;
+        }
+        return false;
     }
 }
