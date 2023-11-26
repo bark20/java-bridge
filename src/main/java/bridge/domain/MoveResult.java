59dad7d9419689e61672da397adae2f93ee4aa6d
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-26 16:18:49 +0900

    refactor(MoveResult) : Bridge -> MoveResult로 클래스명 변경

--- src/main/java/bridge/domain/MoveResult.java
+++ src/main/java/bridge/domain/MoveResult.java
@@ -3,17 +3,13 @@ package bridge.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bridge {
+public class MoveResult {
     private final List<String> upperBridge;
     private final List<String> lowwerBridge;
 
-    private Bridge() {
+    public MoveResult() {
         this.upperBridge = new ArrayList<>();
         this.lowwerBridge = new ArrayList<>();
-    }
-
-    public static Bridge getInstance() {
-        return Holder.INSTANCE;
     }
 
     public void moveUp(String moveResult) {
@@ -26,12 +22,16 @@ public class Bridge {
         lowwerBridge.add(moveResult);
     }
 
-    private static class Holder {
-        private static final Bridge INSTANCE = new Bridge();
+    public boolean isSuccessful() {
+        if (upperBridge.contains("X") || lowwerBridge.contains("X")) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public String toString() {
-        return "[ " + String.join(" | ", upperBridge) + " ]\n" + "[ " + String.join(" | ", lowwerBridge) + " ]";
+        return "[ " + String.join(" | ", upperBridge) + " ]\n" +
+                "[ " + String.join(" | ", lowwerBridge) + " ]";
     }
 }
