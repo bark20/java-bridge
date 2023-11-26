6034d1f30f0934929b9f64abb85c40b8d9829dcc
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-26 16:56:13 +0900

    feat(InputValidator) : 다리 길이에 숫자가 아닌 값 입력시 예외 발생 기능 추가

--- src/main/java/bridge/view/validator/InputValidator.java
+++ src/main/java/bridge/view/validator/InputValidator.java
@@ -1,31 +1,28 @@
 package bridge.view.validator;
 
+import bridge.constants.GameValue;
+import bridge.constants.RegexPattern;
+
 public class InputValidator {
     private static final String ERROR_PREFIX = "[ERROR] ";
-    private static final String MOVE_UP_COMMAND = "U";
-    private static final String MOVE_DOWN_COMMAND = "D";
-    private static final String RETRY_COMMAND = "R";
-    private static final String QUIT_COMMAND = "Q";
-    private static final int MIN_BRIDGE_SIZE = 3;
-    private static final int MAX_BRIDGE_SIZE = 20;
 
     private InputValidator() {
     }
 
-    public static void validateBridgeSize(int bridgeSize) {
-        if (!(bridgeSize >= MIN_BRIDGE_SIZE && bridgeSize <= MAX_BRIDGE_SIZE)) {
-            throw new IllegalArgumentException(ERROR_PREFIX + "다리의 길이는 3이상 20이하의 숫자만 입력할 수 있습니다.");
+    public static void validateNumber(String input) {
+        if (!RegexPattern.ONLY_NUMBER.matches(input)) {
+            throw new IllegalArgumentException(ERROR_PREFIX + "다리의 길이는 숫자만 입력할 수 있습니다.");
         }
     }
 
     public static void validateMoveCommand(String command) {
-        if (!command.equals(MOVE_UP_COMMAND) && !command.equals(MOVE_DOWN_COMMAND)) {
+        if (!command.equals(GameValue.MOVE_UP_COMMAND) && !command.equals(GameValue.MOVE_DOWN_COMMAND)) {
             throw new IllegalArgumentException(ERROR_PREFIX + "이동할 칸은 U 또는 D 문자만 입력할 수 있습니다.");
         }
     }
 
     public static void validateRetryCommand(String command) {
-        if (!command.equals(RETRY_COMMAND) && !command.equals(QUIT_COMMAND)) {
+        if (!command.equals(GameValue.RETRY_COMMAND) && !command.equals(GameValue.QUIT_COMMAND)) {
             throw new IllegalArgumentException(ERROR_PREFIX + "재시작/종료는 R 또는 Q 문자만 입력할 수 있습니다.");
         }
     }
