b4d13abc59079d8dd435d191512dab6aebb0a51d
haero77 <shineecard5@gmail.com>
2023-11-26 17:40:02 +0900

    feat: 애플리케이션 실행을 위한 의존성 주입

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,20 @@
 package bridge;
 
+import bridge.controller.GameController;
+import bridge.view.input.InputView;
+import bridge.view.output.OutputView;
+import bridge.view.print.ConsolePrinter;
+import bridge.view.print.Printer;
+
 public class Application {
 
     public static void main(String[] args) {
+        Printer printer = new ConsolePrinter();
+        InputView inputView = new InputView(printer);
+        OutputView outputView = new OutputView(printer);
+
+        GameController gameController = new GameController(inputView, outputView);
+        gameController.run();
     }
 
 }
