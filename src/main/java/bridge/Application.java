5c92a6ac1acfb538ead3e23e978f26ff59d0e0a8
Hyeon0208 <dmsgktn0208@gmail.com>
2023-11-26 17:45:11 +0900

    feat(Application) : 다리 건너기 게임 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,32 @@
 package bridge;
 
+import bridge.controller.BridgeController;
+import bridge.domain.BridgeGame;
+import bridge.domain.MoveResult;
+import bridge.view.ErrorView;
+import bridge.view.handler.InputHandler;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = initHandler();
+        BridgeGame bridgeGame = InitBridgeGame();
+        OutputView outputView = new OutputView();
+        BridgeController bridgeController = new BridgeController(inputHandler, outputView, bridgeGame);
+        bridgeController.start();
+    }
+
+    private static InputHandler initHandler() {
+        InputView inputView = new InputView();
+        ErrorView errorView = new ErrorView();
+        return  new InputHandler(inputView, errorView);
+    }
+
+    private static BridgeGame InitBridgeGame() {
+        MoveResult moveResult = new MoveResult();
+        int defaultRetryCount = 1;
+        return new BridgeGame(moveResult, defaultRetryCount);
     }
 }
--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -5,6 +5,8 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
+import bridge.domain.BridgeMaker;
+import bridge.domain.BridgeNumberGenerator;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import org.junit.jupiter.api.Test;
