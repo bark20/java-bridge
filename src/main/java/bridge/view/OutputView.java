e6d3bb32160509984228dca4ce4ba65386953579
LEE-sh1673 <lsh901673@gmail.com>
2023-12-03 18:27:23 +0900

    feat: 게임의 결과를 출력하는 기능 추가
    - 게임 시작 문구를 출력한다.
    - 게임 종료 문구를 출력한다.
    - 예외 상황 시 에러 문구를 출력해야 한다.

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,41 @@
 package bridge.view;
 
+import bridge.model.BridgeGame;
+import bridge.model.BridgeType;
+import bridge.model.GameResult;
+import java.util.List;
+import java.util.StringJoiner;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    public void printBridgeGameTitle() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(final GameResult gameResult) {
+        StringBuilder message = new StringBuilder();
+        message.append(formatBridgeOf(gameResult, BridgeType.LEFT));
+        message.append(System.lineSeparator());
+        message.append(formatBridgeOf(gameResult, BridgeType.RIGHT));
+        System.out.println(message);
+    }
+
+    private String formatBridgeOf(final GameResult gameResult, final BridgeType type) {
+        final List<String> moveResults = gameResult.getResultOf(type);
+        final StringJoiner sj = new StringJoiner(" | ", "[ ", " ]");
+
+        for (final String moveResult : moveResults) {
+            sj.add(moveResult);
+        }
+        return sj.toString();
     }
 
     /**
@@ -18,6 +43,17 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(final BridgeGame game) {
+        System.out.println("\n최종 게임 결과");
+        printMap(game.getResult());
+        System.out.println("게임 성공 여부: " + formatGameStatus(game));
+        System.out.println("총 시도한 횟수: " + game.tryCount());
+    }
+
+    private static String formatGameStatus(final BridgeGame game) {
+        if (game.isClear()) {
+            return "성공";
+        }
+        return "실패";
     }
 }
