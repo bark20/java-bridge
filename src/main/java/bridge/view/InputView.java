9db9c759b3b52089944ab7c17ddc19cf83772a4f
LEE-sh1673 <lsh901673@gmail.com>
2023-12-03 18:26:41 +0900

    feat: 사용자로부터 값을 입력받는 기능 추가
    - 다리의 길이를 입력받는다.
    - 플레이어가 이동할 칸을 입력 받는다.
    - 게임 재시작/종료 여부를 입력받는다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,12 @@
 package bridge.view;
 
+import bridge.model.BridgeSize;
+import bridge.model.GameCommand;
+import bridge.model.MoveDirection;
+import bridge.validator.NumericValidator;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.function.Supplier;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,21 +15,45 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public BridgeSize readBridgeSize() {
+        return readWithRetry(() -> {
+            System.out.println("\n다리의 길이를 입력해주세요.");
+            return BridgeSize.withSize(readNumber());
+        });
+    }
+
+    private static int readNumber() {
+        final String strNumber = Console.readLine();
+        NumericValidator.validate(strNumber);
+        return Integer.parseInt(strNumber);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public MoveDirection readMoving() {
+        return readWithRetry(() -> {
+            System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
+            return MoveDirection.findBySymbol(Console.readLine());
+        });
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public GameCommand readGameCommand() {
+        return readWithRetry(() -> {
+            System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+            return GameCommand.findByCode(Console.readLine());
+        });
+    }
+
+    private <T> T readWithRetry(final Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readWithRetry(supplier);
+        }
     }
 }
