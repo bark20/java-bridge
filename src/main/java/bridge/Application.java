7a6d041dc3659de6b26e8b8d774a47e754088ca0
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2023-12-04 15:20:52 +0900

    feat: Application에 Controller 적용

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,12 @@
 package bridge;
 
+import bridge.controller.BridgeController;
+
 public class Application {
+    private static final BridgeController controller = new BridgeController();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        controller.process();
     }
 }
