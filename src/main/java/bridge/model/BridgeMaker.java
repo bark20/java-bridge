f1ceee1e2adbfb74880b44caa20d588116bd83df
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2023-12-04 15:20:12 +0900

    feat: BridgeMaker makeBridge 메서드 정의

--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -1,5 +1,6 @@
-package bridge;
+package bridge.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +19,16 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            int generatedNumber = bridgeNumberGenerator.generate();
+            if (generatedNumber == 1){
+                bridge.add("U");
+                continue;
+            }
+            bridge.add("D");
+        }
+
+        return bridge;
     }
 }
