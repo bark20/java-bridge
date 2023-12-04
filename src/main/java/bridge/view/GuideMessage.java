5f686fef0d2d269a190c9008c9c5141e00d2f9fd
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2023-12-04 15:19:34 +0900

    refactor: GuideMessage 형식 수정

--- src/main/java/bridge/view/GuideMessage.java
+++ src/main/java/bridge/view/GuideMessage.java
@@ -6,8 +6,8 @@ public enum GuideMessage {
     INPUT_NEXT_DIRECTION("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     INPUT_RETRY_OR_NOT("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     OUTPUT_GAME_RESULT("최종 게임 결과"),
-    OUTPUT_GAME_SUCCESS_OR_NOT("게임 성공 여부: "),
-    OUTPUT_TOTAL_TRIAL_COUNT("총 시도 횟수: ")
+    OUTPUT_GAME_SUCCESS_OR_NOT("게임 성공 여부: %s\n"),
+    OUTPUT_TOTAL_TRIAL_COUNT("총 시도 횟수: %d\n")
     ;
 
     private final String message;
