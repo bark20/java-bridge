68d89347921b87de9c2727441249e91af6bdacb2
yoonjuho <dbswn990@naver.com>
2023-12-05 17:53:13 +0900

    feat(BridgeUnit): 무작위 값으로 해당 칸이 위칸인지 아래칸인지 구분할 수 있다.

--- src/main/java/bridge/model/BridgeUnit.java
+++ src/main/java/bridge/model/BridgeUnit.java
@@ -1,8 +1,11 @@
 package bridge.model;
 
+import java.util.Arrays;
+
 public enum BridgeUnit {
     TOP("U", 1),
-    BOTTOM("D", 0),;
+    BOTTOM("D", 0),
+    ;
 
     private String signatureLetter;
     private int signatureNumber;
@@ -10,5 +13,12 @@ public enum BridgeUnit {
     BridgeUnit(String signatureLetter, int signatureNumber) {
         this.signatureLetter = signatureLetter;
         this.signatureNumber = signatureNumber;
+    }
+
+    public static BridgeUnit of(int signatureNumber) {
+        return Arrays.stream(values())
+                .filter(bridgeUnit -> bridgeUnit.signatureNumber == signatureNumber)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("전달된 숫자에 해당하는 칸이 없습니다."));
     }
 }
