a6bfa856b2c17600f08d5058081eb03aca2a0a65
yoonjuho <dbswn990@naver.com>
2023-12-05 21:21:15 +0900

    feat(BridgeGame): 다리를 건널 수 있음과 없음을 표시할 수 있다

--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,16 +1,25 @@
-package bridge;
+package bridge.model;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private Bridge bridge;
+
+    public BridgeGame(Bridge bridge) {
+        this.bridge = bridge;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public String move(String bridgeUnitLetter) {
+        if(bridge.cross(bridgeUnitLetter)){
+            return "O";
+        }
+        return "X";
     }
 
     /**
---
+++ src/test/java/bridge/model/BridgeGameTest.java
@@ -0,0 +1,24 @@
+package bridge.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BridgeGameTest {
+
+    @Test
+    @DisplayName("다리를 건널 수 있음과 없음을 표시할 수 있다")
+    void move(){
+        BridgeGame bridgeGame = new BridgeGame(new Bridge(List.of("U","D","D")));
+        assertAll(
+                () -> assertThat(bridgeGame.move("U")).isEqualTo("O"),
+                () -> assertThat(bridgeGame.move("U")).isEqualTo("X")
+        );
+
+    }
+
+}
