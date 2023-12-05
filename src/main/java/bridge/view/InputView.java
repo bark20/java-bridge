272157dbacdf5a4519ac1a2ed56a0ec24f06fb36
yoonjuho <dbswn990@naver.com>
2023-12-05 22:04:40 +0900

    feat(InputView): 게임 재시작/종료 여부를 입력 받는다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -37,7 +37,11 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String gameCommand = Console.readLine();
+        validateNullAndEmpty(gameCommand);
+        validateSingleLetter(gameCommand);
+        return gameCommand;
     }
 
     private void validateNullAndEmpty(String input) {
