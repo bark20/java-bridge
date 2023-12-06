7cae7d9592ff1daa06579f085ae2e49e8880c752
Hanjaemo <hjm2530@gmail.com>
2023-12-06 16:18:47 +0900

    feat: 최종 결과 출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -37,6 +37,18 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(String bridgeStatus, boolean isSuccess, int tryCount) {
+        System.out.println(bridgeStatus);
+        System.out.println();
+        printSuccessOrFailure(isSuccess);
+        System.out.println(tryCount);
+    }
+
+    private void printSuccessOrFailure(boolean isSuccess) {
+        if (isSuccess) {
+            System.out.println("성공");
+            return;
+        }
+        System.out.println("실패");
     }
 }
