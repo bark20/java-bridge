51c1f427e07bb996e0c9906b8b5efeb888dd7fad
Hanjaemo <hjm2530@gmail.com>
2023-12-06 15:49:11 +0900

    feat: 다리 이동 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,30 @@
 package bridge;
 
+import bridge.domain.Direction;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final List<String> bridge;
+    private int moveCount;
+
+    public BridgeGame(List<String> bridge) {
+        this.bridge = bridge;
+        this.moveCount = 0;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(Direction direction) {
+        boolean movable = bridge.get(moveCount).equals(direction.getName());
+        moveCount++;
+        return movable;
     }
 
     /**
