5aea42800ca7d042bd71e2f31d35a0946f1fa071
kcw0360 <kcw0360@naver.com>
2023-12-12 00:25:11 +0900

    feat(BridgeMaker): 다리 생성

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,9 @@
 package bridge;
 
+import bridge.domain.MoveCommand;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +21,10 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        return IntStream
+                .generate(bridgeNumberGenerator::generate)
+                .limit(size)
+                .mapToObj(MoveCommand::convertCommand)
+                .collect(Collectors.toList());
     }
 }
