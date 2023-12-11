a6aff28d29d966e2841d14f5f2cd54dfd3070deb
yechop <channetattoo@gmail.com>
2023-12-11 19:22:58 +0900

    feat: 다리 길이 입력 - 3 부터 20 사이의 수가 아니면 예외 구현

--- src/main/java/bridge/domain/Validator.java
+++ src/main/java/bridge/domain/Validator.java
@@ -4,7 +4,7 @@ public class Validator {
 
     public void validateBridgeSize(String userInput) {
         validateOnlyNumber(userInput);
-
+        validateNumberRange(Integer.parseInt(userInput));
     }
 
     private void validateOnlyNumber(String userInput) {
@@ -12,6 +12,12 @@ public class Validator {
             if (!Character.isDigit(c)) {
                 throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.");
             }
+        }
+    }
+
+    private void validateNumberRange(int i) {
+        if (!(i >= 3 || i <= 20)) {
+            throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
         }
     }
 }
