42647510614088b70f38d382e677eee28a48eaac
kcw0360 <kcw0360@naver.com>
2023-12-12 01:51:53 +0900

    feat(ViewMessage): 출력 메시지 작성

---
+++ src/main/java/bridge/view/ViewMessage.java
@@ -0,0 +1,21 @@
+package bridge.view;
+
+public enum ViewMessage {
+    START_MESSAGE("다리 건너기 게임을 시작합니다."),
+    INPUT_BRIDGE_SIZE_MESSAGE("다리의 길이를 입력해주세요."),
+    INPUT_MOVE_COMMAND_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    INPUT_RETRY_COMMAND_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    RESULT_MESSAGE("최종 게임 결과"),
+    SUCCESS_OR_FAIL("게임 성공 여부: "),
+    TOTAL_ATTEMPTS_COUNT("총 시도한 횟수: ");
+
+    private final String message;
+
+    ViewMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
