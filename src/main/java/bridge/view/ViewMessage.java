c7a6043b6d3bd16cdba79927a197db958c922006
kcw0360 <kcw0360@naver.com>
2023-12-12 22:46:09 +0900

    feat(ViewMessage): 출력 메시지 작성

--- src/main/java/bridge/view/ViewMessage.java
+++ src/main/java/bridge/view/ViewMessage.java
@@ -6,8 +6,10 @@ public enum ViewMessage {
     INPUT_MOVE_COMMAND_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     INPUT_RETRY_COMMAND_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     RESULT_MESSAGE("최종 게임 결과"),
-    SUCCESS_OR_FAIL("게임 성공 여부: "),
-    TOTAL_ATTEMPTS_COUNT("총 시도한 횟수: ");
+    SUCCESS_OR_FAIL("게임 성공 여부: %s%n"),
+    TOTAL_ATTEMPTS_COUNT("총 시도한 횟수: %d%n"),
+    DELIMITER(" | "),
+    RESULT_FORMAT("[ %s ]%n");
 
     private final String message;
 
