0ff85a548acd730f7691f2a6ee6631cc599ec255
kcw0360 <kcw0360@naver.com>
2023-12-12 22:44:38 +0900

    test(GameDashboardTest): 이동할 칸 기록 객체 생성 테스트

---
+++ src/test/java/bridge/domain/GameDashboardTest.java
@@ -0,0 +1,23 @@
+package bridge.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameDashboardTest {
+
+    @Test
+    @DisplayName("이동할 칸 체크 메소드 테스트")
+    void updateResult() {
+        GameDashboard gameDashboard = new GameDashboard();
+        gameDashboard.updateResult(Status.SUCCESS, MoveCommand.UP);
+        gameDashboard.updateResult(Status.FAIL, MoveCommand.DOWN);
+
+        assertThat(gameDashboard.getUpResult())
+                .isEqualTo(Arrays.asList(Status.SUCCESS, Status.SPACE));
+        assertThat(gameDashboard.getDownResult())
+                .isEqualTo(Arrays.asList(Status.SPACE, Status.FAIL));
+    }
+}
