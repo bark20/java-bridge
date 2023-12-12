8055c286f1d167db4cbd3f42f4251787fe613699
kcw0360 <kcw0360@naver.com>
2023-12-12 22:49:12 +0900

    fear(Bridge): 상태 체크 메소드 작성

--- src/main/java/bridge/domain/model/Bridge.java
+++ src/main/java/bridge/domain/model/Bridge.java
@@ -1,7 +1,9 @@
-package bridge.domain;
+package bridge.domain.model;
 
 import bridge.BridgeMaker;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.constant.MoveCommand;
+import bridge.domain.constant.Status;
 import bridge.exception.InputException;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +20,17 @@ public class Bridge {
 
     public static Bridge createBridge(int size) {
         return new Bridge(size);
+    }
+
+    public int getSize() {
+        return bridge.size();
+    }
+
+    public Status getStatus(int index, MoveCommand moveCommand) {
+        if (bridge.get(index).equals(moveCommand.getCommand())) {
+            return Status.SUCCESS;
+        }
+        return Status.FAIL;
     }
 
     public void setBridge(int size) {
