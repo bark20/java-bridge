ae08f399173d5781258a9c27d67fabc34921c093
MingyeomKim <mingyum119@naver.com>
2023-12-12 19:44:08 +0900

    feat: `다리의 길이를 입력해주세요.` 출력하는 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import bridge.view.console.ConsoleWriter;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,8 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        ConsoleWriter.printlnMessage("다리의 길이를 입력해주세요.");
+
     }
 
     /**
