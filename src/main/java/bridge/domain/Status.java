188eec96e9edea28ab2c8faadbe316dfeaaf9d40
kcw0360 <kcw0360@naver.com>
2023-12-12 22:45:13 +0900

    feat(Status): 성공, 실패 상태관리

---
+++ src/main/java/bridge/domain/Status.java
@@ -0,0 +1,23 @@
+package bridge.domain;
+
+public enum Status {
+    SUCCESS("O", "성공"),
+    FAIL("X", "실패"),
+    SPACE(" ", "");
+
+    private final String status;
+    private final String message;
+
+    Status(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
