7a1d7cdc027a262feebbf97b8dd9d4d10a0871c2
MingyeomKim <mingyum119@naver.com>
2023-12-12 21:48:15 +0900

    feat: 기능 구현 목록 체크 및 게임 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,19 @@
 package bridge;
 
+import bridge.controller.BridgeGame;
+import bridge.domain.BridgeMaker;
+import bridge.util.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeGame bridgeGame = new BridgeGame(
+                new InputView(),
+                new OutputView(),
+                new BridgeMaker(new BridgeRandomNumberGenerator())
+        );
+        bridgeGame.run();
     }
 }
