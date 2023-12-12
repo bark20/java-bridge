cfb112cc4c801fbf5e8284859568b7eca025d5a9
MingyeomKim <mingyum119@naver.com>
2023-12-12 19:26:17 +0900

    feat: 게임 시작 문구를 출력하는 기능 구현

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -1,9 +1,25 @@
-package bridge;
+package bridge.controller;
+
+import bridge.view.InputView;
+import bridge.view.OutputView;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeGame(InputView _inputView, OutputView _outputView) {
+        this.inputView = _inputView;
+        this.outputView = _outputView;
+        outputView.printStartMessage();
+    }
+
+    public void run() {
+
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,4 @@
-package bridge;
+package bridge.view;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
