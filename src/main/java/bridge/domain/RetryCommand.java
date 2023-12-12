0e508edbdb93901c54f3d46e654ab6ad1e5511d4
kcw0360 <kcw0360@naver.com>
2023-12-12 11:02:58 +0900

    feat(RetryCommand): 재시작 커맨드 관련 enum 작성

---
+++ src/main/java/bridge/domain/RetryCommand.java
@@ -0,0 +1,23 @@
+package bridge.domain;
+
+import bridge.exception.InputException;
+import java.util.Arrays;
+
+public enum RetryCommand {
+    RETRY("R"),
+    QUIT("Q");
+
+    private final String command;
+
+    RetryCommand(String command) {
+        this.command = command;
+    }
+
+    public static RetryCommand from(String input) {
+        return Arrays.stream(RetryCommand.values())
+                .filter(com -> com.command.equals(input))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(
+                        InputException.INVALID_RETRY_COMMAND.getMessage()));
+    }
+}
