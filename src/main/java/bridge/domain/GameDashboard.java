a78a21d237950d70e8b16fbcc6c25d046aa95a4c
kcw0360 <kcw0360@naver.com>
2023-12-12 22:44:21 +0900

    feat(GameDashboard): 이동할 칸 기록 객체 생성

---
+++ src/main/java/bridge/domain/GameDashboard.java
@@ -0,0 +1,39 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class GameDashboard {
+
+    private final List<Status> upResult = new ArrayList<>();
+    private final List<Status> downResult = new ArrayList<>();
+
+    public void updateResult(Status status, MoveCommand moveCommand) {
+        if (status == Status.SUCCESS) {
+            checkResult(status, moveCommand);
+        }
+        if (status == Status.FAIL) {
+            checkResult(status, moveCommand);
+        }
+    }
+
+    private void checkResult(Status status, MoveCommand moveCommand) {
+        if (moveCommand == MoveCommand.UP) {
+            upResult.add(status);
+            downResult.add(Status.SPACE);
+        }
+        if (moveCommand == MoveCommand.DOWN) {
+            upResult.add(Status.SPACE);
+            downResult.add(status);
+        }
+    }
+
+    public List<Status> getUpResult() {
+        return Collections.unmodifiableList(upResult);
+    }
+
+    public List<Status> getDownResult() {
+        return Collections.unmodifiableList(downResult);
+    }
+}
