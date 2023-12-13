8468ccdc115342d6ac39de9cb7681dc3cf3a4bea
happyryan <tjdgks120813@gmail.com>
2023-12-13 21:38:19 +0900

    feat(InputView): implement move (U/D) and game commands (R/Q) input handling
    - Added methods for continuous user input of move direction (U/D) and game commands (R/Q)
    - Utilized input handling methods with validation for a more user-friendly experience.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,11 +1,6 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
-import utils.Parser;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class InputView {
     // case1. 단일 숫자만 입력
@@ -38,13 +33,11 @@ public class InputView {
         }
     }
 
-    // ============================ //
-    // case2. 단일 문자열만 입력
-    public static String readString() {
-        System.out.println("시스템 시작");
+    public static String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         while (true) {
             try {
-                String string = inputString();
+                String string = inputMoving();
                 return string;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -52,105 +45,49 @@ public class InputView {
         }
     }
 
-    private static String inputString() {
+    private static String inputMoving() {
         String string = Console.readLine();
-        validateString(string);
+        validateMoving(string);
         return string;
     }
 
-    private static void validateString(String string) {
+    private static void validateMoving(String string) {
         // 영어만 허용하는 정규식
         if (!string.matches("^[a-zA-z]+$")) {
             throw new IllegalArgumentException("[ERROR] 영어만 허용됩니다.");
         }
-//        // 한글만 허용하는 정규식
-//        if (!string.matches("^[가-힣]+$")) {
-//            throw new IllegalArgumentException("[ERROR] 한글만 허용됩니다.");
-//        }
         // 특정 문자만 허용하는 정규식(U와 D만 허용)
         if (!string.equals("U") || string.equals("D")) {
             throw new IllegalArgumentException("[ERROR] U와 D만 허용됩니다.");
         }
     }
 
-    // ============================ //
-    // case3. 구분자로 문자를 입력 받는 경우 - String
-    public static List<String> readStringList() {
-        System.out.println("시스템 시작");
+    public static String readGameCommand() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         while (true) {
             try {
-                List<String> stringList = inputStringList();
-                return stringList;
+                String string = inputGameCommand();
+                return string;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    // 쉼표(,) + 공백 불허를 기준으로 구분 (ex) 김성한,포비,해피 / 김성한
-    private static List<String> inputStringList() {
-        List<String> stringList = List.of(Console.readLine().split(","));
-        validateStringList(stringList);
-        return stringList;
+    private static String inputGameCommand() {
+        String string = Console.readLine();
+        validateGameCommand(string);
+        return string;
     }
 
-    private static void validateStringList(List<String> stringList) {
-        Set<String> uniqueNames = new HashSet<>();
-
-        for (String string : stringList) {
-            // 문자열의 길이 제한 확인
-            if (string.length() > 5) {
-                throw new IllegalArgumentException("[ERROR] 예시를 참고해주세요. 문자열의 길이는 5 미만 입니다.");
-            }
-            // (영어 대소문자 + 숫자 + 한글) 만 허용
-            if (!string.matches("^[a-zA-Z0-9가-힣]+$")) {
-                throw new IllegalArgumentException("[ERROR] 특수문자 및 공백은 불허합니다.");
-            }
-            // 이미 나타난 이름인지 확인 - 중복체크
-            if (!uniqueNames.add(string)) {
-                throw new IllegalArgumentException("[ERROR] 중복된 것이 존재합니다.");
-            }
+    private static void validateGameCommand(String string) {
+        // 영어만 허용하는 정규식
+        if (!string.matches("^[a-zA-z]+$")) {
+            throw new IllegalArgumentException("[ERROR] R와 Q만 허용됩니다.");
         }
-    }
-
-    // ============================ //
-    // case4. 구분자로 숫자를 입력 받는 경우 - Integer
-    public static List<Integer> readIntList() {
-        System.out.println("시스템 시작");
-        while (true) {
-            try {
-                List<Integer> intList = inputIntList();
-                return intList;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-    }
-
-    // 쉼표(,)를 기준으로 구분 (ex) 1,2,3 / 1
-    private static List<Integer> inputIntList() {
-        List<String> stringList = List.of(Console.readLine().split(","));
-        validateIntList(stringList);
-        List<Integer> intList = Parser.parseInteger(stringList);
-        return intList;
-    }
-
-    private static void validateIntList(List<String> intList) {
-        Set<String> uniqueNames = new HashSet<>();
-
-        for (String number : intList) {
-            // 숫자 판단
-            if (!number.matches("^[0-9]+$")) {
-                throw new IllegalArgumentException("[ERROR] 숫자만 허용됩니다.");
-            }
-            // 특정수 기준 이하, 미만, 이상, 초과 판단
-            if (Integer.parseInt(number) > 5) {
-                throw new IllegalArgumentException("[ERROR] 5 이상만 허용됩니다.");
-            }
-            // 중복 판단
-            if (!uniqueNames.add(number)) {
-                throw new IllegalArgumentException("[ERROR] 중복된 것이 존재합니다.");
-            }
+        // 특정 문자만 허용하는 정규식(U와 D만 허용)
+        if (!string.equals("R") || string.equals("Q")) {
+            throw new IllegalArgumentException("[ERROR] R와 Q만 허용됩니다.");
         }
     }
 }
