5f366c40eda087b3f85d2d5ce304031df1f448e0
happyryan <tjdgks120813@gmail.com>
2023-12-14 00:12:35 +0900

    feat(BridgeGameController): implement bridge game controller
    - Added BridgeGameController class responsible for controlling the bridge game flow
    - Implemented run method for running the game loop until completion or user quits
    - Utilized BridgeGame class for managing a single game session
    - Implemented play method for executing the main game logic
      - Uses InputView to read user inputs for moving and game commands
      - Updates and prints the game state through OutputView
      - Handles game end conditions and prints final results.

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -1,2 +1,61 @@
-package bridge;public class BridgeGameController {
+package bridge;
+
+import view.InputView;
+import view.OutputView;
+
+import java.util.*;
+
+public class BridgeGameController {
+
+    private final List<String> bridge;
+
+    private List<String> finalUpBridge;
+    private List<String> finalDownBridge;
+    private int tryCount = 0;
+    public BridgeGameController(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    public void run(){
+        while(true){
+            boolean isGameEnd = play();
+            tryCount++;
+            if(isGameEnd){
+                OutputView.printGameEndMessage();
+                OutputView.printResult(finalUpBridge, finalDownBridge);
+                OutputView.printGameStateMessage(isGameEnd);
+                OutputView.printGameTryMessage(tryCount);
+                return;
+            }
+            String gameCommand = InputView.readGameCommand(); // gameCommand 선언을 이동
+            System.out.println(gameCommand);
+            if(gameCommand.equals("Q")){
+                OutputView.printGameEndMessage();
+                OutputView.printResult(finalUpBridge, finalDownBridge);
+                OutputView.printGameStateMessage(isGameEnd);
+                OutputView.printGameTryMessage(tryCount);
+                return;
+            }
+        }
+    }
+
+    private boolean play() {
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        for (int location = 0; location < bridge.size(); location++) {
+            String moving = InputView.readMoving();
+            System.out.println(moving);
+            boolean isMove = bridgeGame.move(moving, location);
+            OutputView.printMap(bridgeGame.getUpBridge());
+            OutputView.printMap(bridgeGame.getDownBridge());
+            System.out.println();
+            if (!isMove) {
+                finalUpBridge = bridgeGame.getUpBridge();
+                finalDownBridge = bridgeGame.getDownBridge();
+                return false;
+            }
+        }
+        finalUpBridge = bridgeGame.getUpBridge();
+        finalDownBridge = bridgeGame.getDownBridge();
+        return true;
+    }
 }
